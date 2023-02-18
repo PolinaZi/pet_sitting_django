c4824610ac0631453b28b434092c73c243c9885e
Polina Brigadirenko <polina.brigadirenko@mail.ru>
2023-02-18 19:17:01 +0300

    + models

--- web/models.py
+++ web/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+
+class Locality(models.Model):
+    name = models.CharField(max_length=256)
+
+
+class Pet(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Post(models.Model):
+    content = models.TextField()
+    title = models.CharField(max_length=150)
+    post_date = models.DateTimeField()
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    opened = models.BooleanField(default=True)
+    price = models.IntegerField()
+    pets = models.ManyToManyField(Pet)
+    locality = models.ForeignKey(Locality, on_delete=models.SET_NULL)
