3e49a78328749cff2f776d6b9d4eb5047f4ad8ed
Polina Brigadirenko <polina.brigadirenko@mail.ru>
2023-02-25 21:54:35 +0300

    + verbose_name

--- web/models.py
+++ web/models.py
@@ -9,20 +9,20 @@ class Locality(models.Model):
 
 
 class Pet(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField()
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    image = models.ImageField(upload_to='pets/', null=True, blank=True)
+    name = models.CharField(max_length=100, verbose_name='Имя')
+    description = models.TextField(verbose_name='Описание')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Пользователь')
+    image = models.ImageField(upload_to='pets/', null=True, blank=True, verbose_name='Фотография')
 
 
 class Post(models.Model):
-    content = models.TextField()
-    title = models.CharField(max_length=150)
-    post_date = models.DateTimeField()
-    start_date = models.DateTimeField()
-    end_date = models.DateTimeField()
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    opened = models.BooleanField(default=True)
-    price = models.IntegerField()
-    pets = models.ManyToManyField(Pet)
+    content = models.TextField(verbose_name='Содержание')
+    title = models.CharField(max_length=150, verbose_name='Заголовок')
+    post_date = models.DateTimeField(verbose_name='Дата создания')
+    start_date = models.DateTimeField(verbose_name='Дата начала передержки')
+    end_date = models.DateTimeField(verbose_name='Дата окончания передержки')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Пользователь')
+    opened = models.BooleanField(default=True, verbose_name='Открыт')
+    price = models.IntegerField(verbose_name='Цена')
+    pets = models.ManyToManyField(Pet, verbose_name='Питомцы')
     locality = models.ForeignKey(Locality, null=True, on_delete=models.SET_NULL)
