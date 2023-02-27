e1e9b524724d334bda5429c6543e7616d079a72f
Polina Brigadirenko <polina.brigadirenko@mail.ru>
2023-02-27 17:43:06 +0300

    + debug.py

--- petsitting/settings.py
+++ petsitting/settings.py
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "petsitting.debug.SqlPrintingMiddleware"
 ]
 
 ROOT_URLCONF = 'petsitting.urls'
